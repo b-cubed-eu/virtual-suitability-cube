e623f77e743cca781849c8a137e68b67c94022f1
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2025-11-04 15:41:54 +0100

    Update aggregate_suitability.R

--- R/aggregate_suitability.R
+++ R/aggregate_suitability.R
@@ -11,29 +11,29 @@
 
 #' @return A stars R object with suitability as attribute and cells and species as dimension
 
-aggregate_suitability <- function(stars_obj, grid) {
+aggregate_suitability = function(stars_obj, grid) {
   
   # extract species name
-  species_names <- st_get_dimension_values(stars_obj, "species")
+  species_names = st_get_dimension_values(stars_obj, "species")
   
-  agg_list <- vector("list", length(species_names))
+  agg_list = vector("list", length(species_names))
   
   for (i in seq_along(species_names)) {
-    species_name <- species_names[i]
+    species_name = species_names[i]
     message("Aggregating suitability for: ", species_name)
     
     # extract suitability for the species
-    species_raster <- stars_obj["suit", , , i, drop = TRUE]
+    species_raster = stars_obj["suit", , , i, drop = TRUE]
     
     # aggregate over grid by mean
-    agg_suit <- aggregate(species_raster, grid, mean, as_points = TRUE, na.action = na.omit)
+    agg_suit = aggregate(species_raster, grid, mean, as_points = TRUE, na.action = na.omit)
     
     # save
-    agg_list[[i]] <- agg_suit
+    agg_list[[i]] = agg_suit
   }
   
   # combine everything into a stars object
-  aggregated_cube <- do.call(c, agg_list) %>%
+  aggregated_cube = do.call(c, agg_list) %>%
     st_redimension() %>%
     st_set_dimensions(., which = "new_dim", values = species_names, names = "species") %>%
     setNames("suitability")
