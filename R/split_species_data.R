6b0591335e8563a3923d7bfea7e00261d6f8d26f
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2025-11-04 15:44:46 +0100

    Update split_species_data.R

--- R/split_species_data.R
+++ R/split_species_data.R
@@ -7,7 +7,7 @@
 #' scientificName column, the standard format given by gbif
 #' @return A list of n dataframes, one per species
 
-split_species_data <- function(occ_data) {
+split_species_data = function(occ_data) {
   
   # check if the dataset has 'scientificName' column
   if(!"scientificName" %in% colnames(occ_data)) {
@@ -15,11 +15,11 @@ split_species_data <- function(occ_data) {
   }
   
   # species list
-  species_list <- split(occ_data, occ_data$scientificName)
+  species_list = split(occ_data, occ_data$scientificName)
   
   # iteration
   for (species in names(species_list)) {
-    species_name <- gsub(" ", "_", species)  # replace spaces with "_"
+    species_name = gsub(" ", "_", species)  # replace spaces with "_"
     assign(species_name, species_list[[species]], envir = .GlobalEnv)
   }
   
