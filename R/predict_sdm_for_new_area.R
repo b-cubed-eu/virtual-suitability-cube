3c4e58a79889e13b972e1b26df2bcda756d71a35
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2025-11-04 15:44:23 +0100

    Update predict_sdm_for_new_area.R

--- R/predict_sdm_for_new_area.R
+++ R/predict_sdm_for_new_area.R
@@ -10,30 +10,30 @@
 #' @return A stars R object with suitability as attribute and x, y, and species as dimension
 
 
-predict_sdm_for_new_area <- function(models, new_stack) {
-  predictions <- list()
+predict_sdm_for_new_area = function(models, new_stack) {
+  predictions = list()
   
   for (species_name in names(models)) {
     message("SDM for: ", species_name)
     
     # extract model for species 
-    mx <- models[[species_name]]
+    mx = models[[species_name]]
     
     # apply to new stack
-    pred_map <- predictEnmSdm(mx, new_stack)
+    pred_map = predictEnmSdm(mx, new_stack)
     
     # rename layer as 'suitability'
-    names(pred_map) <- "suitability"
+    names(pred_map) = "suitability"
     
     # save
-    predictions[[species_name]] <- pred_map
+    predictions[[species_name]] = pred_map
   }
   
   # from list to stack
-  prediction_stack <- rast(predictions)
+  prediction_stack = rast(predictions)
   
   # from stack to rast
-  stars_predictions <- st_as_stars(prediction_stack) %>% 
+  stars_predictions = st_as_stars(prediction_stack) %>% 
     setNames("suit") %>% 
     st_set_dimensions(., names = c("x", "y", "species"))
   
