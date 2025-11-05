776a34b89c8ffc8b75c79306d9eba351b8e0f5f0
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2025-11-05 15:26:47 +0100

    Update vsc_make_grid_over.R

--- R/vsc_make_grid_over.R
+++ R/vsc_make_grid_over.R
@@ -13,25 +13,25 @@
 #'
 #' @examples
 #' \dontrun{
-#' cl_bel <- vsc_build_worldclim("BEL", month = 5)
-#' grd <- vsc_make_grid_over(cl_bel$predictors, n = c(50, 50))
+#' cl_bel = vsc_build_worldclim("BEL", month = 5)
+#' grd = vsc_make_grid_over(cl_bel$predictors, n = c(50, 50))
 #' }
 #' @export
 #' @importFrom sf st_bbox st_as_sfc st_crs st_make_grid st_sf
 #' @importFrom stars st_as_stars
-vsc_make_grid_over <- function(x, cellsize = NULL, n = c(50, 50), square = TRUE) {
+vsc_make_grid_over = function(x, cellsize = NULL, n = c(50, 50), square = TRUE) {
   # bbox + crs robusti per diversi tipi
-  bb  <- sf::st_bbox(x)
-  sfc <- sf::st_as_sfc(bb)
-  crs <- sf::st_crs(x)
+  bb  = sf::st_bbox(x)
+  sfc = sf::st_as_sfc(bb)
+  crs = sf::st_crs(x)
 
-  pg <- if (!is.null(cellsize)) {
+  pg = if (!is.null(cellsize)) {
     sf::st_make_grid(sfc, cellsize = cellsize, what = "polygons", square = square)
   } else {
     sf::st_make_grid(sfc, n = n,                what = "polygons", square = square)
   }
-  g <- sf::st_sf(geometry = pg)
-  sf::st_crs(g) <- crs
-  g$id <- seq_len(nrow(g))
+  g = sf::st_sf(geometry = pg)
+  sf::st_crs(g) = crs
+  g$id = seq_len(nrow(g))
   g
 }
