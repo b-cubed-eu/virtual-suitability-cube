c32568c2489e74cd2f9b3dc499b63ea82372f7b9
Rocìo Beatriz Cortès <63868353+rociobeatrizc@users.noreply.github.com>
2025-11-05 15:25:04 +0100

    Update split_species_data.R

--- R/split_species_data.R
+++ R/split_species_data.R
@@ -3,7 +3,7 @@
 #' @param occ_data A `data.frame` with at least `scientificName`, `decimalLatitude`, `decimalLongitude`.
 #' @return A named `list(data.frame, ...)` with one element per species.
 #' @export
-split_species_data <- function(occ_data) {
+split_species_data =function(occ_data) {
   if (!"scientificName" %in% names(occ_data)) {
     stop("`occ_data` must contain a 'scientificName' column.", call. = FALSE)
   }
